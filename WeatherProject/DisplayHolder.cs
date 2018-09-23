8b07bb7aa3430a02446ac3f474826b9d671b7af4
HannaMcKay <38221004+HannaMcKay@users.noreply.github.com>
2018-09-23 20:51:43 +0200

    Add files via upload

---
+++ WeatherProject/DisplayHolder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WeatherProject
+
+    /**
+     * For better structur of the code, because json gives more than one class.
+     * Holds the important ui objects.
+     * **/
+{
+    class DisplayHolder
+    {
+        public string DisplayTemp { get; set; }
+        public string DisplayName { get; set; }
+        public string DisplayTempMax { get; set; }
+        public string DisplayTempMin { get; set; }
+        public string DisplayDescription { get; set; }
+        public ImageSource DisplayWeatherImage { get; set; }
+
+        public DisplayHolder() { }
+    }
+}
---
+++ WeatherProject/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherProject
+
+    /**
+     * Main Page to get the api and combine withe the Objects with the DisplayHolder
+     * 
+     * Further down I give two links for the api.
+     * The first one is an example link from the website to create the app.
+     * The second one is a real api link with the weather data from my home town. But you are only allowed to use it once each 10 minutes,
+     * because it is forbidden to use it to sell the app.
+     * */
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); 
+            DownloadWeatherDataAsync();
+        }
+
+        private async void DownloadWeatherDataAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?id=2842647&APPID=aa7f8c0c6727ac8784e731c8529fd60e");
+            //Example link for London with static weather: https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
+            //Real weather in Saarbrucken: http://api.openweathermap.org/data/2.5/weather?id=2842647&APPID=aa7f8c0c6727ac8784e731c8529fd60e
+
+            WeatherRootObject weatherRootObject = JsonConvert.DeserializeObject<WeatherRootObject>(response);
+
+            //Downloadlink for WeatherIcon
+            string pngUrl = "http://openweathermap.org/img/w/" + weatherRootObject.weather[0].icon + ".png";
+
+            //Creating the Objects for de data binding in the xaml file
+            DisplayHolder displayHolder = new DisplayHolder();
+            displayHolder.DisplayTemp           = "  Temperature: "+ (int)(weatherRootObject.main.temp - 273.0) + "°C";
+            displayHolder.DisplayName           = weatherRootObject.name + "";
+            displayHolder.DisplayTempMin        = "Temperature: " + (int)(weatherRootObject.main.temp_min - 273.0) + "°C";
+            displayHolder.DisplayTempMax        = "Temperature: " + (int)(weatherRootObject.main.temp_max - 273.0) + "°C";
+            displayHolder.DisplayDescription    = weatherRootObject.weather[0].description.ToUpper();
+            displayHolder.DisplayWeatherImage   = ImageSource.FromUri(new Uri(pngUrl));
+            BindingContext = displayHolder;
+        }
+    }
+}
